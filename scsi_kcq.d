7c0eaa1d9afbc9860ac274702444cf33867e5b24
Alek P <pinchuk.alek@gmail.com>
2015-12-04 23:06:29 +0000

    added CDDL license

--- scsi_kcq.d
+++ scsi_kcq.d
@@ -1,5 +1,26 @@
 #!/usr/sbin/dtrace -qCs
-/* scsi_kcq.d - A script to print scsi sense data (errors) as it's received, with useful
+/* 
+ *
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
+ * scsi_kcq.d - A script to print scsi sense data (errors) as it's received, with useful
  *           details such as execname issuing the failing command, what the command is,
  *           which device the command reached, what the KCQ is and the failed commands CDB.
  *           Written using DTrace (NexentaOS_134f)
